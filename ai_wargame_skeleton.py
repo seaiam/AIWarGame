fefe3d42a4fcb1cbbb1c4a2a8d9119104576bdb3
Raya Maria Lahoud <rayamarialahoud@gmail.com>
2023-09-28 20:26:13 -0400

    added command line arg for max turns
    Added command line argument for maximum number of turn

--- ai_wargame_skeleton.py
+++ ai_wargame_skeleton.py
@@ -646,6 +646,7 @@ def main():
     parser.add_argument('--max_time', type=float, help='maximum search time')
     parser.add_argument('--game_type', type=str, default="manual", help='game type: auto|attacker|defender|manual')
     parser.add_argument('--broker', type=str, help='play via a game broker')
+    parser.add_argument('--max_turns', type=int, help='maximum turns' )
     args = parser.parse_args()
 
     # parse the game type
@@ -668,6 +669,8 @@ def main():
         options.max_time = args.max_time
     if args.broker is not None:
         options.broker = args.broker
+    if args.max_turns is not None:
+        options.max_turns = args.max_turns
 
     # create a new game
     game = Game(options=options)
